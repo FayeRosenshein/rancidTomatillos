fae53ec07f65812febcb7704747457e676ae3a9c
Faye Rosenshein <rosefaye.a@gmail.com>
2023-02-03 15:30:16 -0700

    Update from main

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,5 +1,16 @@
-// function fetchData(path) {
-//   fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${path}`)
-// }
 
-// export default fetchData;
+
+const fetchAllMovies = () => {
+  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+    .then(response => response.json())
+}
+
+
+const fetchSingleMovie = (id) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+    .then(response => response.json())
+}
+
+
+
+export { fetchAllMovies, fetchSingleMovie };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react"
 
 const Header = () => {
@@ -6,16 +5,3 @@ const Header = () => {
 }
 
 export default Header
-=======
-import React from 'react';
-import './Header.css';
-
-export default function Header() {
-  return (
-    <header>
-        <img alt="tomato icon"/>
-        <h1>Rancid Tomatillos</h1>
-    </header>
-  )
-}
->>>>>>> 41acca2cfa3f947c5aa4834018d44415f8ab841c
