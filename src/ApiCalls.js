f0423d643acae66a64074a295496b8dea1119619
Sarah Hampton <stoluba@gmail.com>
2023-02-08 16:22:38 -0500

    Add error handling to ApiCalls

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -5,7 +5,13 @@ const fetchAllMovies = () => {
 
 const fetchSingleMovie = (id) => {
   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-    .then(response => response.json())
+    .then(response => {
+      if(response.ok)response.json();
+      else throw new Error(response.status)
+
+      
+      
+    })
 }
 
 export { fetchAllMovies, fetchSingleMovie };
