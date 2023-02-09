2da8ed3541995282f9f2ad6d1c74cbe51f14eee9
Faye Rosenshein <rosefaye.a@gmail.com>
2023-02-09 14:25:10 -0700

    Add more functionality to search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,30 +1,11 @@
-import React, { useState } from 'react'
-import { Link } from "react-router-dom";
-import MovieCard from "../MovieCard/MovieCard";
+import React from 'react'
 
-export default function SearchBar({ allMovieInfo, setMovieId }) {
-	const [searchInput, setSearchInput] = useState("")
+export default function SearchBar({ searchInput, setSearchInput }) {
+	
 
 	const handleChange = (e) => {
-		e.preventDefault()
+		// e.preventDefault()
 		setSearchInput(e.target.value)
-	}
-	if (searchInput.length > 0) {
-		const filteredMovies = allMovieInfo.filter(movie => {
-			movie.title.match(searchInput)
-			return (
-				<Link to={`/${movie.id}`} key={movie.title}>
-					<MovieCard
-						key={movie.id}
-						id={movie.id}
-						image={movie.backdrop_path}
-						rating={movie.average_rating}
-						title={movie.title}
-						setMovieId={setMovieId}
-					/>
-				</Link>
-			)
-		})
 	}
 	return (
 		<div>
