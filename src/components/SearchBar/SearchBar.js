495c202f01abb3c6d70b07727b1cd7fb7c4d9973
Faye Rosenshein <rosefaye.a@gmail.com>
2023-02-09 09:50:39 -0700

    Create searchBar component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import { Link } from "react-router-dom";
+import MovieCard from "../MovieCard/MovieCard";
+
+export default function searchBar({allMovieInfo}) {
+	const [searchInput, setSearchInput] = useState("")
+
+	const handleChange = (e) => {
+		e.preventDefault()
+		setSearchInput(e.target.value)
+	}
+	if (searchInput.length > 0) {
+		allMovieInfo.filter(movie => {
+			movie.title.match(searchInput)
+			return (
+			<Link to={`/${movie.id}`} key={movie.title}>
+				<MovieCard
+					key={movie.id}
+					id={movie.id}
+					image={movie.backdrop_path}
+					rating={movie.average_rating}
+					title={movie.title}
+					setMovieId={setMovieId}
+				/>
+			</Link>
+			)
+		})
+	}
+	return (
+		<div>
+		<input 
+			type='text'
+			placeholder='Search Here'
+			onChange={handleChange}
+			value={searchInput}/>
+			
+			</div>
+	)
+}
